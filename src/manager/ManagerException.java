a4607c677a8ce1aa75ed65a4cb26f776f2a457bb
Pavel Stezhkin <sprt4758@gmail.com>
2024-04-25 15:56:29 +0300

    create FileBackedTaskManagerTest

---
+++ src/manager/ManagerException.java
@@ -0,0 +1,9 @@
+package manager;
+
+import java.io.IOException;
+
+public class ManagerException extends IOException {
+    public ManagerException(String message) {
+        super(message);
+    }
+}
--- src/task/SubTask.java
+++ src/task/SubTask.java
@@ -1,5 +1,7 @@
 package task;
 
+import java.util.Objects;
+
 public class SubTask extends Task {
     private final Epic epic;
 
@@ -24,5 +26,17 @@ public class SubTask extends Task {
         return "task.Task{" + "name='" + getName() + '\'' + ", description='" + getDescription() + '\'' + ", id=" + getId() + ", status=" + getStatus() + "}";
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        SubTask subTask = (SubTask) o;
+        return epic.equals(subTask.epic);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), epic);
+    }
 }
